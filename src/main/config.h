2b9f59a80b3aa35b410ddb3f8943cd1f1f4b3a21
Conner.Will <conner.will@infinitecampus.com>
2024-03-21 14:00:52 -0500

    Removed #ifndef guard

--- src/main/config.h
+++ src/main/config.h
@@ -1,6 +1,4 @@
 #pragma once
-#ifndef CONFIG_H
-#define CONFIG_H
 
 const char* const WIFI_SSID     = "YourWifiSSID";     // WiFi SSID (network name)
 const char* const WIFI_PASSWORD = "YourWifiPassword"; // WiFi password
@@ -9,5 +7,3 @@ const int ANALOG_PIN            = 35;                 // Analog pin for CO2 meas
 const int DHT_PIN               = 27;                 // DHT sensor pin for temperature and humidity measurement
 const int WEBSERVER_PORT        = 80;                 // Webserver port
 const int BAUD_RATE             = 115200;             // Baud rate for serial connection
-
-#endif
