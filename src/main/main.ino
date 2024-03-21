fa35b5d9b9acd0930b3a714d47eba9e70ce2c394
Conner.Will <conner.will@infinitecampus.com>
2024-03-21 12:04:53 -0500

    Removed commented code

--- src/main/main.ino
+++ src/main/main.ino
@@ -4,15 +4,6 @@
 #include "ESPAsyncWebServer.h"
 #include "DHTesp.h"
 
-// CONSTANTS --------------------------------------------------------------------
-// const char* const WIFI_SSID     = "YourWifiSSID";     // WiFi SSID (network name)
-// const char* const WIFI_PASSWORD = "YourWifiPassword"; // WiFi password
-// const char* const WIFI_HOSTNAME = "ESP32-plant";      // Hostname for ESP32
-// const int ANALOG_PIN            = 35;                 // Analog pin for CO2 measurement
-// const int DHT_PIN               = 27;                 // DHT sensor pin for temperature and humidity measurement
-// const int WEBSERVER_PORT        = 80;                 // Webserver port
-// const int BAUD_RATE             = 115200;             // Baud rate for serial connection
-
 // OBJECTS -----------------------------------------------------------------------
 DHTesp dht;                            // Initialize the DHT sensor object
 AsyncWebServer server(WEBSERVER_PORT); // Initialize the AsyncWebServer object on port
