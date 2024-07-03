e34139c28d57001759e2552bd79ed950cb1328b8
ConnerWill <conner.will@outlook.com>
2024-07-03 13:23:29 -0500

    ran black

--- grafana/scripts/setup.py
+++ grafana/scripts/setup.py
@@ -1,22 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='esp32-data-collector',
-    version='1.0.0',
-    description='Fetches data from ESP32 sensor and stores it in InfluxDB',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    author='Conner Will',
-    author_email='conner.will@example.com',
+    name="esp32-data-collector",
+    version="1.0.0",
+    description="Fetches data from ESP32 sensor and stores it in InfluxDB",
+    long_description=open(file="../README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Conner Will",
+    author_email="conner.will@example.com",
     packages=find_packages(),
     install_requires=[
-        'requests',
-        'influxdb'
+        "requests",
+        "influxdb",
     ],
-    entry_points={
-        'console_scripts': [
-            'esp32_data_collector = main:main'
-        ]
-    },
+    entry_points={"console_scripts": ["esp32_data_collector = main:main"]},
 )
-
