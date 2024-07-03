5bf564bb597985ee682d95b43ed7ec8e3c1bf4f5
ConnerWill <conner.will@outlook.com>
2024-07-03 13:02:38 -0500

    Ran black, added type info to some vars

--- grafana/scripts/fetch_data.py
+++ grafana/scripts/fetch_data.py
@@ -10,14 +10,17 @@ import time
 esp32_ip = os.getenv("ESP32_IP", "http://<ESP32_IP>")
 
 # InfluxDB configuration
-influxdb_host = os.getenv("INFLUXDB_HOST", 'influxdb')
-influxdb_port = int(os.getenv("INFLUXDB_PORT", 8086))
-influxdb_user = os.getenv("INFLUXDB_USER", 'admin')
-influxdb_password = os.getenv("INFLUXDB_PASSWORD", 'admin')
-influxdb_dbname = os.getenv("INFLUXDB_DBNAME", 'esp32_data')
+influxdb_host: str = os.getenv("INFLUXDB_HOST", "influxdb")
+influxdb_port: int = int(os.getenv("INFLUXDB_PORT", 8086))
+influxdb_user: str = os.getenv("INFLUXDB_USER", "admin")
+influxdb_password: str = os.getenv("INFLUXDB_PASSWORD", "admin")
+influxdb_dbname: str = os.getenv("INFLUXDB_DBNAME", "esp32_data")
 
 # Initialize InfluxDB client
-client = InfluxDBClient(influxdb_host, influxdb_port, influxdb_user, influxdb_password, influxdb_dbname)
+client = InfluxDBClient(
+    influxdb_host, influxdb_port, influxdb_user, influxdb_password, influxdb_dbname
+)
+
 
 def fetch_and_store_data():
     try:
@@ -28,14 +31,12 @@ def fetch_and_store_data():
         json_body = [
             {
                 "measurement": "sensor_data",
-                "tags": {
-                    "device": "esp32"
-                },
+                "tags": {"device": "esp32"},
                 "fields": {
-                    "temperature": data['temperature'],
-                    "humidity": data['humidity'],
-                    "co2": data['co2']
-                }
+                    "temperature": data["temperature"],
+                    "humidity": data["humidity"],
+                    "co2": data["co2"],
+                },
             }
         ]
 
@@ -44,6 +45,7 @@ def fetch_and_store_data():
         print(f"Data written to InfluxDB: {json_body}")
     except Exception as e:
         print(f"Error: {e}")
+
 
 if __name__ == "__main__":
     while True:
