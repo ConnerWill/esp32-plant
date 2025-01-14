023b2ed66af511b7363536f76d27073031a5b662
ConnerWill <conner.will@outlook.com>
2025-01-14 10:47:46 -0600

    Lowercased the hostname in config

--- src/main/config.h
+++ src/main/config.h
@@ -2,7 +2,7 @@
 
 const char* const WIFI_SSID     = "YourWifiSSID";     // WiFi SSID (network name)
 const char* const WIFI_PASSWORD = "YourWifiPassword"; // WiFi password
-const char* const WIFI_HOSTNAME = "ESP32-plant";      // Hostname for ESP32
+const char* const WIFI_HOSTNAME = "esp32-plant";      // Hostname for ESP32
 const int CO2_ANALOG_PIN        = 35;                 // Analog pin for CO2 measurement
 const int DHT_PIN               = 27;                 // DHT sensor pin for temperature and humidity measurement
 const int SOIL_ANALOG_PIN       = 36;                 // Analog pin for soil moisture measurement
