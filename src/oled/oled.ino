1251332fe60ae2e0c213e0adbab05574c8e606e7
ConnerWill <conner.will@outlook.com>
2025-01-21 15:03:21 -0600

    Formatted configs

--- src/oled/oled.ino
+++ src/oled/oled.ino
@@ -17,11 +17,11 @@
 // CONFIGURATION --------------------------------------------------------------
 // ============================================================================
 // WIFI CONFIGURATION
-constexpr char WIFI_SSID[]       = "YourNetworkName"; // Wi-Fi SSID
-constexpr char WIFI_PASSWORD[]   = "YourNetworkPass"; // Wi-Fi password
-constexpr char WIFI_HOSTNAME[]   = "esp32-oled";      // Hostname
-constexpr int  WIFI_TIMEOUT_TIME = 30000;             // Timeout if unable to connect to WiFi (ms)
-const unsigned long WIFI_CHECK_INTERVAL = 5000;       // Check WiFi time (ms)
+constexpr char WIFI_SSID[]         = "YourNetworkName"; // Wi-Fi SSID
+constexpr char WIFI_PASSWORD[]     = "YourNetworkPass"; // Wi-Fi password
+constexpr char WIFI_HOSTNAME[]     = "esp32-oled";      // Hostname
+constexpr int  WIFI_TIMEOUT_TIME   = 30000;             // Timeout if unable to connect to WiFi (ms)
+constexpr int  WIFI_CHECK_INTERVAL = 5000;              // Check WiFi time (ms)
 
 // SERVER CONFIGURATION
 constexpr uint16_t SERVER_PORT = 80;    // Port for the web server
@@ -32,11 +32,11 @@ constexpr uint8_t CO2_PIN = 35; // Analog pin for CO2 sensor
 constexpr uint8_t DHT_PIN = 27; // GPIO pin for DHT sensor
 
 // SCREEN CONFIGURATION
-constexpr int SCREEN_WIDTH                = 128;  // OLED display width, in pixels
-constexpr int SCREEN_HEIGHT               = 64;   // OLED display height, in pixels
-constexpr uint8_t SCREEN_ADDRESS          = 0x3C; // Address of OLED display (could also be '0x3D' depending on screen resolution)
-constexpr int SCREEN_UPDATE_TIME          = 1000; // Time to wait before updating OLED (ms)
-constexpr int SCREEN_STARTUP_DISPLAY_TIME = 3000; // Startup screen delay time (ms)
+constexpr int     SCREEN_WIDTH                = 128;  // OLED display width, in pixels
+constexpr int     SCREEN_HEIGHT               = 64;   // OLED display height, in pixels
+constexpr uint8_t SCREEN_ADDRESS              = 0x3C; // Address of OLED display (could also be '0x3D' depending on screen resolution)
+constexpr int     SCREEN_UPDATE_TIME          = 1000; // Time to wait before updating OLED (ms)
+constexpr int     SCREEN_STARTUP_DISPLAY_TIME = 3000; // Startup screen delay time (ms)
 
 // SERIAL CONFIGURATION
 constexpr int BAUD_RATE = 115200; // Baud rate
