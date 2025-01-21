7030b5ecfc4c6c762613b97f86870abfa59d7eea
ConnerWill <conner.will@outlook.com>
2025-01-21 10:22:16 -0600

    Added oled

--- src/oled/oled.ino
+++ src/oled/oled.ino
@@ -2,6 +2,9 @@
 #include "ESPAsyncWebServer.h"
 #include "DHTesp.h"
 #include <ArduinoJson.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
 // ============================================================================
 // CONFIGURATION --------------------------------------------------------------
@@ -10,18 +13,31 @@ constexpr char WIFI_SSID[]     = "YourNetworkName"; // Wi-Fi SSID
 constexpr char WIFI_PASSWORD[] = "YourNetworkPass"; // Wi-Fi password
 constexpr char WIFI_HOSTNAME[] = "esp32-oled";      // Hostname
 
-constexpr int   SERVER_PORT = 80;  // Port for the web server
-constexpr char  SERVER_PATH[] = "/"; // Path for serving the data
+constexpr int  SERVER_PORT = 80;    // Port for the web server
+constexpr char SERVER_PATH[] = "/"; // Path for serving the data
 
 constexpr int CO2_PIN = 35; // Analog pin for CO2 sensor
 constexpr int DHT_PIN = 27; // GPIO pin for DHT sensor
 
+#define SCREEN_WIDTH 128        // OLED display width, in pixels
+#define SCREEN_HEIGHT 64        // OLED display height, in pixels
+#define SCREEN_UPDATE_TIME 1000 // Time to wait before updating OLED (ms)
+#define SCREEN_ADDRESS 0x3C     // Address of OLED display (could also be '0x3D' depending on screen resolution)
+
+
 constexpr int BAUD_RATE = 115200; // Baud rate
 // ============================================================================
 
-// Global Instances
+// ============================================================================
+// GLOBAL INSTANCES -----------------------------------------------------------
+// ============================================================================
 DHTesp dht;
 AsyncWebServer server(SERVER_PORT);
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+// ============================================================================
 
 // ============================================================================
 // FUNCTIONS ------------------------------------------------------------------
@@ -59,6 +75,50 @@ void connectToWiFi() {
   Serial.printf("Hostname: %s\n", WiFi.getHostname());
 }
 
+// Function to initialize OLED
+void initOLED() {
+  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("Failed to start SSD1306 OLED"));
+    while (1);
+  }
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+  display.display();
+}
+
+// Function to update the OLED with sensor readings
+void updateOLED(int co2, float temperature, float humidity) {
+  display.clearDisplay();
+
+  // IP Address
+  display.setCursor(0, 0);
+  display.print("IP: ");
+  display.println(WiFi.localIP().toString().c_str());
+
+  // Temperature
+  display.setCursor(0, 10);
+  display.print("Temp: ");
+  display.print(temperature);
+  display.println(" C");
+
+  // Humidity
+  display.setCursor(0, 20);
+  display.print("Humidity: ");
+  display.print(humidity);
+  display.println(" %");
+
+  // CO2
+  display.setCursor(0, 30);
+  display.print("CO2: ");
+  display.print(co2);
+  display.println(" ppm");
+
+  // Update Display
+  display.display();
+}
+
 // ============================================================================
 
 void setup() {
@@ -70,6 +130,9 @@ void setup() {
 
   // Initialize Serial for debugging
   Serial.begin(BAUD_RATE);
+
+  // Initialize OLED
+  initOLED();
 
   // Connect to Wi-Fi
   connectToWiFi();
@@ -95,6 +158,10 @@ void setup() {
 
     // Send the JSON response
     request->send(200, "application/json", jsonString);
+
+  // WE MIGHT WANT TO UPDATE THE DISPLAY IN LOOP INSTEAD OF SETUP
+  //  // Update OLED display with latest sensor data
+  //  //updateOLED(co2, temperature, humidity);
   });
 
   // Start the server
@@ -102,4 +169,22 @@ void setup() {
   Serial.println("Server started.");
 }
 
-void loop() {}
+void loop() {
+
+// WE MIGHT WANT TO UPDATE THE DISPLAY IN LOOP INSTEAD OF SETUP
+  // Get sensor values
+  int co2 = getCo2Measurement();
+  float temperature = dht.getTemperature();
+  float humidity = dht.getHumidity();
+
+  // Debug values
+  Serial.printf("Temperature: %s C\n", temperature);
+  Serial.printf("Humidity   : %s %\n", humidity);
+  Serial.printf("co2        : %s ppm\n", co2);
+  Serial.println("Updating display...");
+
+  // Update OLED display with latest sensor data
+  updateOLED(co2, temperature, humidity);
+
+  delay(SCREEN_UPDATE_TIME);
+}
